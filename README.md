561ad7c27e02b96767194aea2f8976098ea13902
Luis Ch <g298luis@gmail.com>
2021-07-08 21:44:39 -0400

    Py file to contracts added

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 This console application can show your staked and pending balances you have on PancakeSwap Staking pool (Binance Smart Chain).
 
-It works with Manual Cake pool only, for now. 
-Modify 'contract' and 'contract_abi' to query other pools.
+I made it to query Manual Cake pool. 
+You can query other pools by modifying `contract` and `contract_abi`.
 
 The GUI looks nice thanks to [Rich](https://github.com/willmcgugan/rich). 
+
+![Web3](https://img.shields.io/badge/-Web3.py-gray?style=flat&logo=ethereum)
+![BSC](https://img.shields.io/badge/-BSC-gray?style=flat&logo=binance)
+![PancakeSwap](https://img.shields.io/badge/-%F0%9F%A5%9E%20PancakeSwap-gray?style=flat)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import time
-from rich import console
+
 from web3 import Web3
-from rich.console import Console
-from rich.table import Table
 from rich import box
+from rich import console
+from rich.table import Table
+from rich.console import Console
+
 import crypto_v1 as crypto
+import pancakeswap
 import res
 
 
 def calc_apr(min):
     init_balance = float(check_pool(res.wallet2)['pending'])
-    print(f'At {time.asctime()} -> {init_balance} tokens')
+    print(f'{time.strftime("%H:%M:%S")} -> {init_balance} tokens')
     time.sleep(min * 60)
     last_check = check_pool(res.wallet2)
     final_balance = float(last_check['pending'])
     staked = float(last_check['staked'])
-    print(f'At {time.asctime()} -> {final_balance} tokens')
+    print(f'{time.strftime("%H:%M:%S")} -> {final_balance} tokens')
     # Calc APR
     yih = (final_balance - init_balance) * (60/min)
     yihp = (yih / staked) * 100
@@ -26,10 +29,10 @@ def calc_apr(min):
 def check_pool(wallet):
     bsc_mainnet = "https://bsc-dataseed3.binance.org/"
     web3 = Web3(Web3.HTTPProvider(bsc_mainnet))
-    print(f'Connected to Web3: {web3.isConnected()}')
+    #print(f'Connected to Web3: {web3.isConnected()}')
     # PancakeSwap: Main Staking Contract (Manual Pool)
-    contract = '0x73feaa1eE314F8c655E354234017bE2193C9E24E'
-    contract_abi = '[{"inputs":[{"internalType":"contract CakeToken","name":"_cake","type":"address"},{"internalType":"contract SyrupBar","name":"_syrup","type":"address"},{"internalType":"address","name":"_devaddr","type":"address"},{"internalType":"uint256","name":"_cakePerBlock","type":"uint256"},{"internalType":"uint256","name":"_startBlock","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"EmergencyWithdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"pid","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"BONUS_MULTIPLIER","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"contract IBEP20","name":"_lpToken","type":"address"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"add","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"cake","outputs":[{"internalType":"contract CakeToken","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cakePerBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"deposit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_devaddr","type":"address"}],"name":"dev","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"devaddr","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"emergencyWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"enterStaking","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_from","type":"uint256"},{"internalType":"uint256","name":"_to","type":"uint256"}],"name":"getMultiplier","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"leaveStaking","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"massUpdatePools","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"migrate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"migrator","outputs":[{"internalType":"contract IMigratorChef","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"pendingCake","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"poolInfo","outputs":[{"internalType":"contract IBEP20","name":"lpToken","type":"address"},{"internalType":"uint256","name":"allocPoint","type":"uint256"},{"internalType":"uint256","name":"lastRewardBlock","type":"uint256"},{"internalType":"uint256","name":"accCakePerShare","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_allocPoint","type":"uint256"},{"internalType":"bool","name":"_withUpdate","type":"bool"}],"name":"set","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract IMigratorChef","name":"_migrator","type":"address"}],"name":"setMigrator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"syrup","outputs":[{"internalType":"contract SyrupBar","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalAllocPoint","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"multiplierNumber","type":"uint256"}],"name":"updateMultiplier","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"}],"name":"updatePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"address","name":"","type":"address"}],"name":"userInfo","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"rewardDebt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_pid","type":"uint256"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+    contract = pancakeswap.manual_pool_contract
+    contract_abi = pancakeswap.manual_pool_contract_abi
     contract_info = web3.eth.contract(contract, abi=contract_abi)
     # Call contract functions to get data
     wallet = web3.toChecksumAddress(wallet)
@@ -79,11 +82,12 @@ def view_stake():
 
 
 if __name__ == '__main__':
-    console = Console()
     init_time = time.perf_counter()
     view_stake()
-    with console.status('Calculando APR, estará listo en 3 min.') as status:
-        apr = calc_apr(min=3)
-        Console().print(f'\nAPR = {round(apr, 2)}%', style="bold green")
     elapsed_time = round(time.perf_counter() - init_time, 2)
     print(f' *** Elapsed Time: {elapsed_time} s ***\n')
+    console = Console()
+    print('Calculating APR...')
+    with console.status('it will take a minute') as status:
+        apr = calc_apr(min=1)
+        Console().print(f'\nAPR = {round(apr, 2)}%', style="bold green")
